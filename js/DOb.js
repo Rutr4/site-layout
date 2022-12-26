4be5933056b78d4b04c6f03e87849b481dec0c6e
Andrew Malinin <a.malinin@skb.rtp>
2022-12-27 02:41:21 +0300

    dragging realised

--- js/DOb.js
+++ js/DOb.js
@@ -15,44 +15,75 @@ const imgParts = [
   },
 ];
 
-const region = document.querySelector(".region");
+const workspace = document.querySelector(".puzzle-workspace");
 
 function generatePartElement(partInfo) {
   const imgContainer = document.createElement("div");
   const img = document.createElement("img");
-  imgContainer.classList.add("chimera_goat", "chimera__part");
-  // imgContainer.style.transform = `rotate(${partInfo.angle}deg)`;
+  imgContainer.classList.add("chimera__part");
+  imgContainer.style.transform = `rotate(${partInfo.angle}deg)`;
   img.src = partInfo.imgSrc;
   img.alt = partInfo.alt;
+  // Чтобы понять зачем это - закомментируй)))
+  img.ondragstart = ()=>false;
   imgContainer.appendChild(img);
-  imgContainer.addEventListener("pointerup", handleMouseDown);
+  imgContainer.addEventListener("pointerdown", (e)=>{
+    handleImgPartCaptured(e, workspace);
+  });
   return imgContainer;
 }
 
 function init() {
   imgParts.forEach((imgPart) => {
-    region.appendChild(generatePartElement(imgPart));
+    workspace.appendChild(generatePartElement(imgPart));
   });
 }
 
-function handleMouseDown(e) {
-  console.log(e);
-  const element = e.target;
-  const x = element.x;
-  const y = element.y;
+const handleImgPartCaptured = (e, workspace = document.body) => {
+  let clickLeftOffset, clickTopOffset = 0;
+  const element = e.currentTarget;
+  if (element === null) return;
+  element.classList.add('chimera__part_state_captured');
+  const workspaceHeight = workspace.getBoundingClientRect().height;
+  const workspaceWidth = workspace.getBoundingClientRect().width;
 
-  function handlePointerMove(e) {
-    const mouseX = e.x;
-    const mouseY = e.y;
+  clickLeftOffset = e.clientX - element.getBoundingClientRect().left;
+  clickTopOffset = e.clientY - element.getBoundingClientRect().top;
 
-    element.style.left = "";
-    elem.style.top = event.pageY - offsetY + "px";
-    //     elem.style.left = event.pageX - offsetX + "px";
+  document.onpointermove = elementDrag;
+  document.onpointerup = handleElementReleased;
+
+  function elementDrag(e) {
+    if (element === null) return;
+    // calculate the new cursor position:
+    const cursorX = e.clientX - workspace.getBoundingClientRect().left;
+    const cursorY = e.clientY- workspace.getBoundingClientRect().top;
+
+    // set the element's new position:
+    const offsetFromEdge = 1; //px
+    const minLeft = offsetFromEdge;
+    const maxLeft = workspaceWidth - element.getBoundingClientRect().width - offsetFromEdge;
+    const minTop = offsetFromEdge;
+    const maxTop = workspaceHeight - element.getBoundingClientRect().height - offsetFromEdge;
+
+    let topShift = cursorY - clickTopOffset;
+    let leftShift = cursorX - clickLeftOffset;
+
+    if (topShift > maxTop) topShift = maxTop;
+    else if (topShift < minTop) topShift = minTop;
+
+    if (leftShift > maxLeft) leftShift = maxLeft;
+    else if (leftShift < minLeft) leftShift = minLeft;
+
+    element.style.top = (topShift / workspaceHeight) * 100 + '%';
+    element.style.left = (leftShift / workspaceWidth) * 100 + '%';
   }
 
-  document.addEventListener("pointermove", handlePointerMove);
+  function handleElementReleased() {
+    document.onpointermove = null;
+    element.classList.remove('chimera__part_state_captured')
+  }
+};
 
-  document.removeEventListener("pointerdown");
-}
 
 init();
