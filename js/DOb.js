fa5f33d35e1929bab669714a168d76fd1a4214cb
Artur <a.muradian439@gmail.com>
2023-01-02 01:33:43 +0300

    123

--- js/DOb.js
+++ js/DOb.js
@@ -1,34 +1,45 @@
+const workspace = document.querySelector(".puzzle-workspace");
+const testBtn = document.getElementsByClassName("js-btn-test");
+// информация об изображениях
+//todo добавить 4ый элемент и изменить изображения
 const imgParts = [
   {
-    x: 50,
-    y: 50,
-    angle: 0,
+    top: 60,
+    left: 350,
+    angle: 90,
     imgSrc: "assets/img/drag_obj/Chimera_goat.png",
     alt: "Химера - изображение головы козы",
   },
   {
-    x: 50,
-    y: 50,
+    top: 10,
+    left: 850,
     angle: 0,
     imgSrc: "assets/img/drag_obj/Chimera_lion.png",
     alt: "Химера - изображение тела льва",
   },
   {
-    x: 50,
-    y: 50,
-    angle: 45,
+    top: 150,
+    left: 50,
+    angle: 0,
     imgSrc: "assets/img/drag_obj/Chimera_snake.png",
     alt: "Химера - изображение хвоста змеи",
   },
 ];
-
-const workspace = document.querySelector(".puzzle-workspace");
-
+// добавление элементов на страницу
+init();
+function init() {
+  imgParts.forEach((imgPart) => {
+    workspace.appendChild(generatePartElement(imgPart));
+  });
+}
 function generatePartElement(partInfo) {
   const imgContainer = document.createElement("div");
   const img = document.createElement("img");
   imgContainer.classList.add("chimera__part");
   imgContainer.style.transform = `rotate(${partInfo.angle}deg)`;
+  imgContainer.style.top = partInfo.top + "px";
+  imgContainer.style.left = partInfo.left + "px";
+
   img.src = partInfo.imgSrc;
   img.alt = partInfo.alt;
   img.ondragstart = () => false;
@@ -38,22 +49,19 @@ function generatePartElement(partInfo) {
   });
   return imgContainer;
 }
-
-function init() {
-  imgParts.forEach((imgPart) => {
-    workspace.appendChild(generatePartElement(imgPart));
-  });
-}
+const parts = document.querySelectorAll(".chimera__part");
 
 const handleImgPartCaptured = (e, workspace = document.body) => {
-  let clickLeftOffset,
-    clickTopOffset = 0;
   const element = e.currentTarget;
   if (element === null) return;
   element.classList.add("chimera__part_state_captured");
+
+  let clickLeftOffset,
+    clickTopOffset = 0;
   const workspaceHeight = workspace.getBoundingClientRect().height;
   const workspaceWidth = workspace.getBoundingClientRect().width;
 
+  // координаты клика относительно верхнего левого края элемента
   clickLeftOffset = e.clientX - element.getBoundingClientRect().left;
   clickTopOffset = e.clientY - element.getBoundingClientRect().top;
 
@@ -62,11 +70,12 @@ const handleImgPartCaptured = (e, workspace = document.body) => {
 
   function elementDrag(e) {
     if (element === null) return;
-    // calculate the new cursor position:
+
+    // получение координат курсора относительно рабочей области
     const cursorX = e.clientX - workspace.getBoundingClientRect().left;
     const cursorY = e.clientY - workspace.getBoundingClientRect().top;
 
-    // set the element's new position:
+    // ограничение перетаскивания элемента рабочей областью
     const offsetFromEdge = 1; //px
     const minLeft = offsetFromEdge;
     const maxLeft =
@@ -75,6 +84,7 @@ const handleImgPartCaptured = (e, workspace = document.body) => {
     const maxTop =
       workspaceHeight - element.getBoundingClientRect().height - offsetFromEdge;
 
+    // отслеживаем сдвиг элемента
     let topShift = cursorY - clickTopOffset;
     let leftShift = cursorX - clickLeftOffset;
 
@@ -84,8 +94,10 @@ const handleImgPartCaptured = (e, workspace = document.body) => {
     if (leftShift > maxLeft) leftShift = maxLeft;
     else if (leftShift < minLeft) leftShift = minLeft;
 
-    element.style.top = (topShift / workspaceHeight) * 100 + "%";
-    element.style.left = (leftShift / workspaceWidth) * 100 + "%";
+    //element.style.top = (topShift / workspaceHeight) * 100 + "%";
+    element.style.top = topShift + "px";
+    //element.style.left = (leftShift / workspaceWidth) * 100 + "%";
+    element.style.left = leftShift + "px";
   }
 
   function handleElementReleased() {
@@ -94,42 +106,16 @@ const handleImgPartCaptured = (e, workspace = document.body) => {
   }
 };
 
-function check() {}
-function rotateOnKey() {
-  for (let i = 0; i < 6; i++) {
-    picture.item(i).onclick = () => {
-      document.onkeydown = () => {
-        rotate(picture.item(i), i, elements[i].cur_angle + 45);
-      };
-    };
-  }
-}
-
-function rotate(pic, i, x) {
-  if (!elements[i].inItsArea) {
-    pic.style.transform = "rotate(" + x + "deg)";
-    elements[i].cur_angle = x;
-  }
-}
-
-function win() {
-  for (let i = 0; i < 6; i++) {
-    picture.item(i).style.border = 0;
-    picture
-      .item(i)
-      .animate(
-        [
-          { transform: "rotate(" + 45 + "deg)" },
-          { transform: "rotate(" + -90 + "deg)" },
-          { transform: "rotate(" + 90 + "deg)" },
-          { transform: "rotate(" + -90 + "deg)" },
-          { transform: "rotate(" + 90 + "deg)" },
-          { transform: "rotate(" + -90 + "deg)" },
-          { transform: "rotate(" + 45 + "deg)" },
-        ],
-        1500
-      );
-  }
-}
-
-init();
+// поворот изображения по двойному клику
+parts.forEach((element) => {
+  element.ondblclick = () => {
+    let angle = element.style.transform;
+    //регулярное выражение, для извлечения ЧИСЛОВОГО значения поворота элемента
+    angle = angle.replace(/[^\d.]/g, "");
+    angle = parseInt(angle);
+    element.style.transform = `rotate(${angle + 90}deg)`;
+    console.log(element.style.transform);
+  };
+});
+// TODO реализовать тест сбора изображения
+// TODO что делать при выигрыше
